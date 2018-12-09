88241f1738dc68e1082ab76e352e32eb263115ea
Joseph Wang <jamesxxxxxxx@gmail.com>
2018-12-09 22:07:30 +0800

    add BgAnimation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ const App = () => {
     <>
       <BgAnimation />
       <GlobalStyle />
-      skdlfmlkwmeflkmwef
     </>
   );
 };
--- src/components/BgAnimation.js
+++ src/components/BgAnimation.js
@@ -1,14 +1,42 @@
 import React, { memo } from 'react';
 import styled from '@emotion/styled/macro';
+import { keyframes } from '@emotion/core';
+import bg from 'images/bg.jpg';
+import { useWindowSize } from 'react-use';
+
+const rotate = keyframes`
+  from {
+    transform: translate(-50%,-50%) rotate(0deg);
+  }
+  to {
+    transform: translate(-50%,-50%) rotate(360deg);
+  }
+`;
 
 const Container = styled.div`
-  position: relative;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  overflow: hidden;
+  background-color: black;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  height: auto;
+  animation: ${rotate} 180s linear infinite;
 `;
 
 const BgAnimation = () => {
+  const { width, height } = useWindowSize();
+
   return (
     <Container>
-      {/* code code code code code code code code code code code code code */}
+      <Image src={bg} style={{ width: (width ** 2 + height ** 2) ** 0.5 }} />
     </Container>
   );
 };
